f5c2914f38a3ea9a99a7904b4eb7314601960313
Michael Seymour <mseymour208@gmail.com>
2026-09-14 14:05:54 -0600

    foundational outline

--- proxy.cpp
+++ proxy.cpp
@@ -2,6 +2,13 @@
 
 void proxy::SocketLoop() {
 
-    
+    // Instantiate epoll instance
+    // Create original socket configured for non-blocking operation
+    // registered with epoll instance
+    // Enter epoll loop
+    // Wait for notification from epoll
+    // accept() a new client socket
+    // extract file descriptor
+    // set to non blocking and register to epoll instance
 
 }
