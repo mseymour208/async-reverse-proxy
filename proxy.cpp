3ef0bca0a17b16664ea71abd209b47f13af044bc
Michael Seymour <mseymour208@gmail.com>
2026-09-14 14:40:47 -0600

    Socket binding

--- proxy.cpp
+++ proxy.cpp
@@ -1,9 +1,25 @@
 #include "proxy.h"
 
-void proxy::SocketLoop() {
+void SocketLoop() {
 
     // Instantiate epoll instance
+    int epoll_fd = epoll_create1(0);
+
     // Create original socket configured for non-blocking operation
+    int original_socket = socket(AF_INET, SOCK_STREAM, O_NONBLOCK);
+    
+    // Binding setup
+    struct sockaddr_in sock_address = {0};
+    // Configuring address
+    sock_address.sin_family = AF_INET;
+    sock_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    sock_address.sin_port = htons(8080);
+
+    // Binding socket to specific network address
+    int bind_status = bind(original_socket, (struct sockaddr *)&sock_address, sizeof(sock_address));
+
+
+
     // registered with epoll instance
     // Enter epoll loop
     // Wait for notification from epoll
@@ -12,3 +28,4 @@ void proxy::SocketLoop() {
     // set to non blocking and register to epoll instance
 
 }
+
--- proxy.h
+++ proxy.h
@@ -2,8 +2,17 @@
 #define PROXY_H
 
 #include <sys/epoll.h>
-#include <stdio>
-using namespace std
+#include <unistd.h>
+#include <sys/socket.h>
+#include <iostream>
+#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <string.h>
+
+using namespace std;
 
 // initializing epoll and socket loop
 void SocketLoop();
