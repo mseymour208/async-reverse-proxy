18942a920dffe81ed6863f66708a1ca690fa9c57
Michael Seymour <mseymour208@gmail.com>
2026-09-21 15:13:15 -0600

    Chunked array insertion & lookup implementation

---
+++ chunked_array.cpp
@@ -0,0 +1,33 @@
+#include "chunked_array.h"
+
+void push_back(const T& fd) {
+    // Where we allocate data
+    int outer_idx = fd / chunk_size;
+    int inner_idx = fd % chunk_size;
+
+    // We don't have outer_idx spaces in outer vector
+    if (ptr_array.size() < outer_idx) {
+        // resize
+        ptr_array.resize(outer_idx, make_unique<Chunk>());
+    }
+
+    // ptr at outer_idx is empty
+    if (ptr_array[outer_idx] == nullptr) {
+        ptr_array.push_back(make_unique<Chunk>());
+    }
+
+    (*ptr_array[outer_idx])[inner_idx] = fd;
+
+}
+
+T& operator[](size_t index) {
+    size_t outer_idx = index / chunk_size;
+    size_t inner_idx = index % chunk_size;
+    return (*ptr_array[outer_idx])[inner_idx];
+}
+
+const T& operator[](size_t index) const {
+    size_t outer_idx = index / chunk_size;
+    size_t inner_idx = index % chunk_size;
+    return (*ptr_array[outer_idx])[inner_idx];
+}
--- chunked_array.h
+++ chunked_array.h
@@ -4,6 +4,18 @@
 #include "proxy.h"
 
 
+template <typename T, size_t chunk_size = 1028>
 class chunked_array {
+    private:
+        using Chunk = array<T, chunk_size>;
 
-}
+        vector<unique_ptr<Chunk>> ptr_array;
+
+    public:
+        chunked_array() = default;
+        void push_back(const T& value);
+        T& operator[](size_t index);
+        const T& operator[](size_t index) const;
+};
+
+#endif
